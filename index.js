bde665647ecf3a8152af0948506ba680dc6fc2ce
JenaDurell <66094197+JenaDurell@users.noreply.github.com>
2020-06-04 20:37:22 -0400

    functioning with BCA recommended binary search algo

--- index.js
+++ index.js
@@ -6,13 +6,48 @@ function ask(questionText) {
     rl.question(questionText, resolve);
   });
 }
+//random number generator
+function randomNum(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+//computer guesses number
+startCompGuessNum();
 
-start();
-
-async function start() {
+async function startCompGuessNum() {
   console.log("Let's play a game where you (human) make up a number and I (computer) try to guess it.")
   let secretNumber = await ask("What is your secret number?\nI won't peek, I promise...\n");
   console.log('You entered: ' + secretNumber);
-  // Now try and complete the program.
-  process.exit();
+
+  //range
+  let high = 100
+  let low = 1
+
+  //computer's guess
+  // let computerGuess = randomNum(low, high)
+  let computerGuess = Math.floor((high-low)/2) + low
+  //myAnswer is Y or N
+  let myAnswer = await ask(`Is it ${computerGuess}, yes (y) or no (n)?`)
+
+
+
+
+   while (myAnswer.toLowerCase() === 'n') {
+    let highLow = await ask('Is it higher (h) or lower (l)?')
+    if (highLow.toLowerCase() === 'h') {
+      low = computerGuess + 1;
+    } else if (highLow.toLowerCase() === 'l') {
+      high = computerGuess - 1
+    }
+    computerGuess = randomNum(low, high)
+    myAnswer = await ask(`Is it ${computerGuess}, yes (y) or no (n)?`)
+
+  }
+  if (myAnswer.toLowerCase() === 'y') {
+
+    console.log('Yes! You win!')
+
+    process.exit();
+
+  }
+
 }
