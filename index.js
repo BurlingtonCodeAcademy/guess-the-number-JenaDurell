2b05715b9681cf9f8b223a0b925cd97395fe3b7a
JenaDurell <66094197+JenaDurell@users.noreply.github.com>
2020-06-07 16:47:33 -0400

    seperate file created; includes refactoring

--- index.js
+++ index.js
@@ -2,3 +2,24 @@ const readline = require('readline');
 const rl = readline.createInterface(process.stdin, process.stdout);
 
 
+//choosing which game to play first
+
+gameChoice()
+async function gameChoice() {
+  let userGameChoice = await ask('Do you want to guess first or should I? \nPress "1" if you want to guess first and press "2" if you want me to guess first. \n')
+    if (parseInt(userGameChoice) === 1) {
+      startCompGuessNum()
+    } else if (parseInt(userGameChoice) === 2) {
+      startHumGuessNum()
+    } else {
+      console.log("Well that's not a choice at all! I guess you don't want to play any of my games :( Goodbye!")
+      process.exit()
+    }
+}
+
+function ask(questionText) {
+  return new Promise((resolve, reject) => {
+    rl.question(questionText, resolve);
+  });
+}
+
