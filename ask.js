8e78e14246abf93bfdd05150e5b13f9d9a3089a8
JenaDurell <66094197+JenaDurell@users.noreply.github.com>
2020-06-07 22:58:07 -0400

    things are working

--- ask.js
+++ ask.js
@@ -1,7 +1,6 @@
 const readline = require('readline');
 const rl = readline.createInterface(process.stdin, process.stdout);
-const humGuess = require('./humguess')
-const compGuess = require('./compguess')
+
 
 function ask(questionText) {
     return new Promise((resolve, reject) => {
@@ -9,13 +8,9 @@ function ask(questionText) {
     });
 }
 
-function newGame(gameName)  {
-    if (gameName === 'comp') {compGuess.startCompGuessNum()}
-    if (gameName === 'hum') {humGuess.startHumGuessNum()}
 
-}
 
 
 
 //share file within project
-module.exports = { ask, newGame }
+module.exports = { ask }
